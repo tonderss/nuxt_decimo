f47aa0cddc07694f1cb3020da70c80e6e0205ebb
Alexis pantoja enriquez <Alexi Pantoja>
2019-03-21 20:26:41 -0500

    iniciando proyecto

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>HOLA {{ msg }}</h1>
+
+    <input type="text" v-model="msg">
+    <button @click="guardar"> guardar</button>
+
+    <table>
+      <tr>
+        <th>nombre</th>
+      </tr>
+      <tr v-for="(item, index) in municipio" :key="index">
+        <td>{{ item }}</td>
+      </tr>
+    
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return { msg: "Eduar Alexi", municipio: ["mocoa", "aqui", "puerto"] 
+    };
+  },
+
+  methods:{
+      guardar(){
+          
+          this.municipio.push(this.msg)
+          this.msg =" "
+
+      }  }
+};
+</script>
